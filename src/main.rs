e074974e17730fe56f72d6d7dfd5f6e43ad74ce1
Esty Thomas <estelendur@gmail.com>
2017-12-03 01:16:20 +0000

    Add templates

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,23 @@
 #![plugin(rocket_codegen)]
 
 extern crate rocket;
+extern crate rocket_contrib;
 extern crate budget_app;
 
+use std::collections::HashMap;
+use rocket_contrib::Template;
+
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> Template {
+    let mut context = HashMap::new();
+    context.insert("greeting", "Hello");
+    Template::render("index", context)
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    let conn = budget_app::establish_connection();
+    rocket::ignite()
+        .mount("/", routes![index])
+        .attach(Template::fairing())
+        .launch();
 }
