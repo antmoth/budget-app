4eb89f46be54a2be7be232847a7e320e53e38fad
Esty Thomas <estelendur@gmail.com>
2017-12-27 17:47:41 -0500

    FormTransaction

--- src/controllers/transaction.rs
+++ src/controllers/transaction.rs
@@ -1,34 +1,47 @@
 use diesel::pg::PgConnection;
-use bigdecimal::BigDecimal;
-use chrono::NaiveDate;
-use diesel::{self, RunQueryDsl};
+use diesel::{self, Connection, RunQueryDsl};
+use rocket::response::Redirect;
+use rocket::request::LenientForm;
 
-use models::account::*;
-use models::category::*;
-use models::payee::*;
 use models::transaction::*;
+use error::Error;
+use context::Context;
 
-pub fn create_transaction<'a>(conn: &PgConnection, date: NaiveDate, account: Account, category: Option<Category>, payee: Option<Payee>, parent_transaction: Option<Transaction>, amount: BigDecimal, memo: Option<&'a str>, cleared: bool) -> Transaction {
+#[post("/new_transaction", data = "<transaction>")]
+pub fn new_transaction_post(context: Context, transaction: LenientForm<FormTransaction>) -> Result<Redirect, Error> {
+    let transaction = transaction.get();
+
+    context.db.transaction(|| {
+        create_transaction(&context.db, &transaction);
+        Ok(Redirect::to("/budget"))
+    })
+    .or_else(|e| Err(e))
+}
+
+pub fn create_transaction<'a>(conn: &PgConnection, transaction: &FormTransaction) -> Transaction {
     use schema::transactions;
 
     let new_transaction = NewTransaction {
-        date: date,
-        account: account.id,
-        category: match category {
-            Some(cat) => Some(cat.id),
+        date: transaction.date.0,
+        account: transaction.account.0,
+        category: match transaction.category {
+            Some(ref u) => Some(u.0),
             _ => None,
         },
-        payee: match payee {
-            Some(pay) => Some(pay.id),
+        payee: match transaction.payee {
+            Some(ref u) => Some(u.0),
             _ => None,
         },
-        parent_transaction: match parent_transaction {
-            Some(tran) => Some(tran.id),
+        parent_transaction: match transaction.parent_transaction {
+            Some(ref u) => Some(u.0),
             _ => None,
         },
-        amount: amount,
-        memo: memo,
-        cleared: cleared,
+        amount: transaction.amount.0.clone(),
+        memo: match transaction.memo {
+            Some(ref s) => Some(&s),
+            _ => None
+        },
+        cleared: transaction.cleared,
     };
 
     diesel::insert_into(transactions::table)
--- src/models/transaction.rs
+++ src/models/transaction.rs
@@ -2,8 +2,11 @@ use uuid::Uuid;
 use bigdecimal::BigDecimal;
 use chrono::NaiveDate;
 use schema::transactions;
+use models::account::Balance;
+use rocket::http::RawStr;
+use rocket::request::FromFormValue;
 
-#[derive(Queryable)]
+#[derive(Queryable, Serialize, Deserialize)]
 pub struct Transaction {
     pub id: Uuid,
     pub date: NaiveDate,
@@ -27,4 +30,42 @@ pub struct NewTransaction<'a> {
     pub amount: BigDecimal,
     pub memo: Option<&'a str>,
     pub cleared: bool,
+}
+
+#[derive(FromForm)]
+pub struct FormTransaction {
+    pub date: FormDate,
+    pub account: FormUuid,
+    pub category: Option<FormUuid>,
+    pub payee: Option<FormUuid>,
+    pub parent_transaction: Option<FormUuid>,
+    pub amount: Balance,
+    pub memo: Option<String>,
+    pub cleared: bool,
+}
+
+pub struct FormDate(pub NaiveDate);
+
+impl<'v> FromFormValue<'v> for FormDate {
+    type Error = &'v str;
+
+    fn from_form_value(form_value: &'v RawStr) -> Result<FormDate, &'v str> {
+        match NaiveDate::parse_from_str(&form_value, "%Y-%m-%d") {
+            Ok(date) => Ok(FormDate(date)),
+            _ => Err("Unable to parse date")
+        }
+    }
+}
+
+pub struct FormUuid(pub Uuid);
+
+impl<'v> FromFormValue<'v> for FormUuid {
+    type Error = &'v str;
+
+    fn from_form_value(form_value: &'v RawStr) -> Result<FormUuid, &'v str> {
+        match Uuid::parse_str(&form_value) {
+            Ok(uuid) => Ok(FormUuid(uuid)),
+            _ => Err("Unable to parse uuid")
+        }
+    }
 }
